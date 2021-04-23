3a5669f0bfca5b6c903eef308f792877596e2057
Adrian <adrian@sharpmind.de>
2021-04-23 15:30:00 +0200

    First commit

--- src/main/kotlin/de/sharpmind/ktor/EnvConfig.kt
+++ src/main/kotlin/de/sharpmind/ktor/EnvConfig.kt
@@ -1,9 +1,12 @@
 package de.sharpmind.ktor
 
-import io.ktor.config.ApplicationConfig
-import io.ktor.config.ApplicationConfigValue
-import io.ktor.util.KtorExperimentalAPI
+import com.typesafe.config.Config
+import com.typesafe.config.ConfigException
+import com.typesafe.config.ConfigFactory
+import io.ktor.config.*
+import io.ktor.util.*
 import org.slf4j.LoggerFactory
+import java.io.File
 
 /**
  * Environment config main class
@@ -15,11 +18,13 @@ object EnvConfig {
     private const val ROOT_NODE = "envConfig"
     private const val ENVIRONMENT_NODE = "env"
     private const val DEFAULT_ENVIRONMENT = "default"
+    private const val EC_CONFIGFILE = "EC_CONFIGFILE"
 
     /* todo  in case of an override use ConfigFactory.load("conf/url_short").withFallback(ConfigFactory.defaultApplication()).resolve()
     private val urlShortConfig: Config =
         ConfigFactory.load("conf/url_short").withFallback(ConfigFactory.defaultApplication()).resolve()*/
     private lateinit var config: ApplicationConfig
+    private lateinit var externalConfig: Config
     private val logger = LoggerFactory.getLogger(this::class.java)
     private var environment: String = DEFAULT_ENVIRONMENT
 
@@ -32,10 +37,12 @@ object EnvConfig {
     fun initConfig(config: ApplicationConfig): EnvConfig =
         apply {
             this.config = config
-
             // if the environment property is available, we set it, if not use default
             this.environment = config.propertyOrNull("$ROOT_NODE.$ENVIRONMENT_NODE")?.getString() ?: DEFAULT_ENVIRONMENT
+
             logger.info("use environment: $environment")
+
+            handleExtraConfigFile()
         }
 
     /**
@@ -51,8 +58,13 @@ object EnvConfig {
      *
      * @return the int property for provided key or null if it doesn't exist
      */
-    fun getInt(propertyKey: String): Int? =
-        getIntInternal(environment, propertyKey) ?: getIntInternal(DEFAULT_ENVIRONMENT, propertyKey)
+    fun getInt(propertyKey: String): Int? {
+        var value: Int? = gerPropertyExternal(environment, propertyKey) as Int
+
+        println("external value ${value}")
+
+        return value ?: (getIntInternal(environment, propertyKey) ?: getIntInternal(DEFAULT_ENVIRONMENT, propertyKey))
+    }
 
     /**
      * Gets list property from config with default fallback
@@ -100,7 +112,7 @@ object EnvConfig {
      * @param defaultVal the returned value, in case of null
      * @return the string property for provided key or defaultVal if it doesn't exist
      */
-    fun getString(propertyKey: String, defaultVal : String): String = getString(propertyKey) ?: defaultVal
+    fun getString(propertyKey: String, defaultVal: String): String = getString(propertyKey) ?: defaultVal
 
     private fun getBooleanInternal(environment: String, propertyKey: String): Boolean? =
         getStringInternal(environment, propertyKey)?.toBoolean()
@@ -124,10 +136,51 @@ object EnvConfig {
         // do we have a config?
         if (!::config.isInitialized) {
             logger.warn("EnvConfig not initialized yet")
+
             throw Exception("Not initialized yet")
         }
 
         val path = "${ROOT_NODE}.${environment}.$propertyKey"
+
         return config.propertyOrNull(path)
+    }
+
+    private fun gerPropertyExternal(environment: String, propertyKey: String): Any? {
+        // do we have a config?
+        if (!::externalConfig.isInitialized) {
+            logger.warn("externalConfig not initialized yet")
+
+            //throw Exception("Not initialized yet")
+        }
+
+        val path = "${ROOT_NODE}.${environment}.$propertyKey"
+
+        try {
+            return externalConfig?.getAnyRef(path)
+        } catch (e: ConfigException.Missing) {
+            logger.warn("required data is was not found")
+        }
+        return null
+    }
+
+    private fun handleExtraConfigFile() {
+        try {
+            val externalConfigFilePath = System.getenv(EC_CONFIGFILE)
+            val configFile: File = File("/u/adrian/test.conf")
+
+            logger.info("externalConfigFilePath is $externalConfigFilePath, configFile is $configFile acess ${configFile.exists()}")
+            if (configFile.exists() && configFile.canRead()) {
+                externalConfig = ConfigFactory.parseFile(configFile)
+
+                logger.info(externalConfig?.toString())
+            }
+        } catch (e: NullPointerException) {
+            logger.warn("no external config file provided")
+        } catch (e: SecurityException) {
+            logger.warn("access denied for the external config file's env. variable")
+        }
+        catch (e : Exception){
+            logger.warn(e.message)
+        }
     }
 }
